562bfe57de3165767333b7c52de0512c4fbffe3c
szymonbrandnewbrand <61582575+szymonbrandnewbrand@users.noreply.github.com>
2020-02-28 14:23:51 +0100

    Revert "fad"
    This reverts commit 155657c522a5d9e60082a8566d79def50987e536.

--- js/kurs.js
+++ js/kurs.js
@@ -1,51 +1,51 @@
 
 $(document).ready(function () {
     $.getJSON("js/data.json", function (os) {
-        const urlParams = new URLSearchParams(window.location.search);
-        const myParam = urlParams.get('id');
-        const rat = urlParams.get('r');
-        let list = [];
-        let data;
-
+    const urlParams = new URLSearchParams(window.location.search);
+    const myParam = urlParams.get('id');
+    const rat = urlParams.get('r');
+    let list = []; 
+    let data;
+        
         for (let key in os) {
             os[key].category = os[key].category.split(" ")
             list.push(os[key]);
         }
-        data = list.filter((e) => e.id == myParam);
-        data[0].rating = Number(rat);
+            data = list.filter((e) => e.id == myParam);
+            data[0].rating = Number(rat);
+            
+        
+    var price = data[0].price;
+    var wal = 'PLN';
+    var img = data[0].img;
+    if(price.length<1){
+        var price = 'brak';
+        var wal = '';
+    }
+    if(img.length<1){
+        var img = 'card-img.png';
+    }
 
+    const stars = Math.floor(data[0].rating)
+    
 
-        var price = data[0].price;
-        var wal = 'PLN';
-        var img = data[0].img;
-        if (data[0].price < 1) {
-            var price = 'brak';
-            var wal = '';
-        }
-        if (data[0].img < 1) {
-            var img = 'card-img.png';
-        }
+    const osRatings = [];
 
-        const stars = Math.floor(data[0].rating)
+    const x = [data[0].rating1, data[0].rating2, data[0].rating3, data[0].rating4, data[0].rating5];
 
-
-        const osRatings = [];
-
-        const x = [data[0].rating1, data[0].rating2, data[0].rating3, data[0].rating4, data[0].rating5];
-
-        createStars = (stars) => {
-            var starImg = ``;
-            for (let i = 0; i < stars; i++) {
-                starImg += /*html */`<img class="star" src="img/star.png">`;
-            }
-
-            return starImg;
+    createStars = (stars) => {
+        var starImg = ``;
+        for (let i = 0; i < stars; i++) {
+            starImg += /*html */`<img class="star" src="img/star.png">`;
         }
 
+        return starImg;
+    }
 
 
 
-        const itemHeader = /*html*/`
+
+    const itemHeader = /*html*/`
         <div class="col-md-5 d-flex os-img p-0 justify-content-center align-items-center">
             <img src="img/${img}">
         </div>
@@ -78,10 +78,10 @@ $(document).ready(function () {
         </div>
     `
 
-        var kategorie = ["Zaangażowanie instruktora", "Efekt szkolenia", "Zaangażowanie ośrodka", "Profesjonalizm instruktora", "Stan samochodów szkoleniowych"];
-        for (let k = 0; k < 5; k++) {
+    var kategorie = ["Zaangażowanie instruktora", "Efekt szkolenia", "Zaangażowanie ośrodka", "Profesjonalizm instruktora", "Stan samochodów szkoleniowych"];
+    for (let k = 0; k < 5; k++) {
 
-            osRatings.push(/*html*/ `
+        osRatings.push(/*html*/ `
             <div class="px-0 mb-4 col-md-6">
                 <p class="mb-0">${kategorie[k]}: </p>
                 <div class="row justify-content-center justify-content-xl-start ml-xl-0">
@@ -90,47 +90,47 @@ $(document).ready(function () {
             </div>
             
     `)
-        }
+    }
 
-        console.log(osRatings);
+    console.log(osRatings);
 
 
-        const ratings = document.getElementById('os-ratings');
+    const ratings = document.getElementById('os-ratings');
 
-        const des = document.getElementById('os-description');
-        const he = document.getElementById('os-card-header');
+    const des = document.getElementById('os-description');
+    const he = document.getElementById('os-card-header');
 
-        const ra = document.createElement("div");
+    const ra = document.createElement("div");
 
-        const cardHeader = document.createElement("div");
-        const descripton = document.createElement("p");
+    const cardHeader = document.createElement("div");
+    const descripton = document.createElement("p");
 
-        const form_text = document.getElementById('form-text');
-        form_text.innerHTML = `Uzupełnij formularz kontaktowy. Ośrodek ${data[0].name} skontaktuje się z Tobą.`
+    const form_text = document.getElementById('form-text');
+    form_text.innerHTML = `Uzupełnij formularz kontaktowy. Ośrodek ${data[0].name} skontaktuje się z Tobą.`
 
-        const mail_osk = document.getElementById('mail_osk');
-        mail_osk.innerHTML = `
+    const mail_osk = document.getElementById('mail_osk');
+    mail_osk.innerHTML = `
     <input type="email" class="invisible_bnb" name="osk" value="${data[0].email}">
     <input type="text" class="invisible_bnb" name="kat" value="${data[0].category}">
     `
 
-        ra.className = "row justify-content-start m-0";
+    ra.className = "row justify-content-start m-0";
 
-        for (let i = 0; i < osRatings.length; i++) {
-            ra.innerHTML += osRatings[i];
-        }
+    for (let i = 0; i < osRatings.length; i++) {
+        ra.innerHTML += osRatings[i];
+    }
 
-        console.log(ra)
-
-
-        cardHeader.className = "row m-0";
-        cardHeader.innerHTML = '';
-        cardHeader.innerHTML += itemHeader;
-        descripton.innerHTML = data[0].description;
+    console.log(ra)
 
 
-        he.appendChild(cardHeader);
-        des.appendChild(descripton);
-        ratings.appendChild(ra);
-    });
+    cardHeader.className = "row m-0";
+    cardHeader.innerHTML = '';
+    cardHeader.innerHTML += itemHeader;
+    descripton.innerHTML = data[0].description;
+
+
+    he.appendChild(cardHeader);
+    des.appendChild(descripton);
+    ratings.appendChild(ra);
+});
 });
